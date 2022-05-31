70e41cc9b9618defb8ceb197d10ba7b0bea94340
gmod916 <benjamin_moon916@hotmail.com>
2022-05-31 22:34:58 +0200

    Update playerkdagroups.sql

--- queries/playerkdagroups.sql
+++ queries/playerkdagroups.sql
@@ -1,3 +1,4 @@
+#all players KDA for group stage 
 SELECT p.nickname, FORMAT((SUM(ps.kills)+SUM(ps.assists))/SUM(ps.deaths),2) AS KDA
 FROM playerstat AS ps
 LEFT JOIN players AS p
@@ -9,7 +10,7 @@ WHERE
         FROM
 			matches
         WHERE 
-			matchdate < '2022-05-20'
+			matchdate <= '2022-05-15'
             )
 GROUP BY p.nickname
 ORDER BY KDA DESC
