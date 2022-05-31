9ae4321dfb3554d4e478741f082f7d9605d97373
gmod916 <benjamin_moon916@hotmail.com>
2022-05-31 22:37:17 +0200

    Update playerkdagroups.sql

--- queries/playerkdagroups.sql
+++ queries/playerkdagroups.sql
@@ -3,9 +3,8 @@ SELECT p.nickname, FORMAT((SUM(ps.kills)+SUM(ps.assists))/SUM(ps.deaths),2) AS K
 FROM playerstat AS ps
 LEFT JOIN players AS p
 ON ps.playerID = p.playerID
-WHERE
-	matchID IN (
-		SELECT matchID
+WHERE matchID IN (
+	SELECT matchID
         FROM matches
         WHERE matchdate <= '2022-05-15'
 	#change matchdate for different stages of the tournament.
