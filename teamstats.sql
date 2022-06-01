155a84525a449ac3e7c9ad1e469c48b35e0704a0
gmod916 <benjamin_moon916@hotmail.com>
2022-06-01 18:34:25 +0200

    Add files via upload

---
+++ teamstats.sql
@@ -0,0 +1,14 @@
+#team stats
+SELECT t.teamabbr AS team, 
+SUM(CASE WHEN t.teamid = m.bluesideID AND m.bluesidewin = 'Y' THEN 1
+WHEN t.teamid = m.redsideID AND m.bluesidewin = 'N' THEN 1 ELSE 0 END) AS wins,
+SUM(CASE WHEN t.teamid = m.bluesideID AND m.bluesidewin = 'N' THEN 1
+WHEN t.teamid = m.redsideID AND m.bluesidewin = 'Y' THEN 1 ELSE 0 END) AS losses,
+CONCAT(ROUND(SUM(CASE WHEN t.teamid = m.bluesideID AND m.bluesidewin = 'Y' THEN 1
+WHEN t.teamid = m.redsideID AND m.bluesidewin = 'N' THEN 1 ELSE 0 END)/COUNT(m.bluesideID)*100),'%') AS winrate
+FROM matches m
+INNER JOIN teams t
+ON m.bluesideID = t.teamid OR m.redsideID = t.teamid
+#WHERE m.matchdate = 
+GROUP BY t.teamid
+ORDER BY wins DESC;
